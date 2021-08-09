9a9c6fbc6a1db7a68b75169c784ee825dd472e28
Janek Ozga <janekozga1@gmail.com>
2021-08-09 17:20:43 +0200

    Create service architecture, create Spotify service to fetch currently playing song

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import '../features/core/layout/styles.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { SpotifyProvider } from '../shared/services/spotify/spotify.service';
+
+const queryClient = new QueryClient();
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const MyApp = ({ Component, pageProps }) => {
     return (
         <React.Fragment>
-            <Component {...pageProps} />
+            <QueryClientProvider client={queryClient}>
+                <SpotifyProvider>
+                    <Component {...pageProps} />
+                </SpotifyProvider>
+                <ReactQueryDevtools initialIsOpen />
+            </QueryClientProvider>
         </React.Fragment>
     );
 };
