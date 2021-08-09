c4ca5212a211327ed9b9ca037c2c37ba9bd69e47
Janek Ozga <janekozga1@gmail.com>
2021-08-09 18:03:07 +0200

    Add logging for github pages deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
  * @type {import('next').NextConfig}
  **/
 module.exports = withGoogleFonts({
-    assetPrefix: isProd ? './' : '',
+    assetPrefix: './',
     googleFonts: {
         fonts: ['https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'],
     },
