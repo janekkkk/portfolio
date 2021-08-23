90c73378caaab997b65026593d6229112e794f4c
Janek Ozga <janekozga1@gmail.com>
2021-08-23 20:54:28 +0200

    Add blog post, add some typewriter texts

--- features/posts/posts.component.tsx
+++ features/posts/posts.component.tsx
@@ -1,118 +1,65 @@
 import React from 'react';
-import { useSpotifyService } from '@/shared/services/spotify/spotify.service';
 
 const posts = [
     {
-        title: 'Boost your conversion rate',
-        href: '#',
-        category: { name: 'Article', href: '#' },
+        title: 'Prettier Linting',
+        href: 'https://medium.com/@janekkkk/prettier-linting-c2aa755e583b',
+        category: { name: 'Article', href: 'https://medium.com/@janekkkk/prettier-linting-c2aa755e583b' },
         description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
-            'Architecto accusantium praesentium eius, ut atque fuga culpa, ' +
-            'similique sequi cum eos quis dolorum.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-        readingTime: '6 min',
-        author: {
-            name: 'Roel Aufderehar',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        title: 'How to use search engine optimization to drive sales',
-        href: '#',
-        category: { name: 'Video', href: '#' },
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
-            'Velit facilis asperiores porro quaerat doloribus, eveniet dolore. ' +
-            'Adipisci tempora aut inventore optio animi., tempore temporibus ' +
-            'quo laudantium.',
-        date: 'Mar 10, 2020',
-        datetime: '2020-03-10',
-        imageUrl:
-            'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+            'Recently I’ve been getting into code quality tools as I’ve just started my graduation internship project. ' +
+            'I wanted to save my findings for future reference but also share it with my new colleagues and the rest of the world.',
+        date: '29 Nov, 2020',
+        datetime: '2017-11-29',
+        imageUrl: 'https://miro.medium.com/max/2000/1*n_AHPX0MrFPveTqDft9VpQ.jpeg',
         readingTime: '4 min',
         author: {
-            name: 'Brenna Goyette',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-    },
-    {
-        title: 'Improve your customer experience',
-        href: '#',
-        category: { name: 'Case Study', href: '#' },
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae ' +
-            'magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-        date: 'Feb 12, 2020',
-        datetime: '2020-02-12',
-        imageUrl:
-            'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-        readingTime: '11 min',
-        author: {
-            name: 'Daniela Metz',
-            href: '#',
-            imageUrl:
-                'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            name: 'Janek Ozga',
+            href: 'https://medium.com/@janekkkk',
+            imageUrl: 'https://miro.medium.com/fit/c/64/64/0*xVkUc83eWIWGw4uA.jpg',
         },
     },
 ];
 
 export default function Posts(): JSX.Element {
     return (
-        <div className="relative pt-16 pb-20 lg:pt-24 lg:pb-28">
-            <div className="">
-                <div className="">
-                    <h2>From the blog</h2>
-                    <p className="mt-3 max-w-2xl text-lg sm:mt-4">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque,
-                        ducimus sed.
-                    </p>
-                </div>
-                <div className="mt-12 grid gap-5 lg:grid-cols-3 lg:max-w-none">
-                    {posts.map((post) => (
-                        <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                            <div className="flex-shrink-0">
-                                <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
+        <div>
+            <div className="grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                {posts.map((post) => (
+                    <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                        <div className="flex-shrink-0">
+                            <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
+                        </div>
+                        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                            <div className="flex-1">
+                                <p className="text-sm font-medium text-indigo-600">
+                                    <a href={post.category.href}>{post.category.name}</a>
+                                </p>
+                                <a href={post.href} className="block mt-2">
+                                    <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                                    <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                                </a>
                             </div>
-                            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                                <div className="flex-1">
-                                    <p className="text-sm font-medium text-indigo-600">
-                                        <a href={post.category.href}>{post.category.name}</a>
-                                    </p>
-                                    <a href={post.href} className="block mt-2">
-                                        <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                                        <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                            <div className="mt-6 flex items-center">
+                                <div className="flex-shrink-0">
+                                    <a href={post.author.href}>
+                                        <span className="sr-only">{post.author.name}</span>
+                                        <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
                                     </a>
                                 </div>
-                                <div className="mt-6 flex items-center">
-                                    <div className="flex-shrink-0">
-                                        <a href={post.author.href}>
-                                            <span className="sr-only">{post.author.name}</span>
-                                            <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
-                                        </a>
-                                    </div>
-                                    <div className="ml-3">
-                                        <p className="text-sm font-medium text-gray-900">
-                                            <a href={post.author.href}>{post.author.name}</a>
-                                        </p>
-                                        <div className="flex space-x-1 text-sm text-gray-500">
-                                            <time dateTime={post.datetime}>{post.date}</time>
-                                            <span aria-hidden="true">&middot;</span>
-                                            <span>{post.readingTime} read</span>
-                                        </div>
+                                <div className="ml-3">
+                                    <p className="text-sm font-medium text-gray-900">
+                                        <a href={post.author.href}>{post.author.name}</a>
+                                    </p>
+                                    <div className="flex space-x-1 text-sm text-gray-500">
+                                        <time dateTime={post.datetime}>{post.date}</time>
+                                        <span aria-hidden="true">&middot;</span>
+                                        <span>{post.readingTime} read</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
