7c01cb56a09582eca792ddcbefa73e3e9f69011d
Janek Ozga <janekozga1@gmail.com>
2021-08-23 16:23:51 +0200

    Add typewriter effect on hero component

--- features/about-me/about.me.component.tsx
+++ features/about-me/about.me.component.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
+
+const TypeWriterEffect = dynamic(
+    () => {
+        return import('react-typewriter-effect');
+    },
+    { ssr: false }
+);
 
 export default function AboutMe({ className = '' }): JSX.Element {
     return (
         <div className={className}>
-            <h1 className="mt-1 font-bold text-3xl md:text-5xl tracking-normal mb-4 text-black dark:text-white">
+            <h1 className="mt-1 font-bold text-3xl md:text-6xl tracking-normal mb-4 text-black dark:text-white">
                 Hey, I&apos;m Janek Ozga
             </h1>
-            <p className="max-w-xl mt-5 text-xl mb-16">
-                Front-end developer, JavaScript enthusiast, tree hugger and avid runner.{' '}
-                <Link href={'/about'}>Learn more about me.</Link>
-            </p>
+            <div className="max-w-xl mt-5 text-xl mb-16">
+                <TypeWriterEffect
+                    textStyle={{
+                        fontSize: '1.5rem',
+                        height: '1.5rem',
+                    }}
+                    startDelay={0}
+                    cursorColor="#3F3D56"
+                    multiText={[
+                        'Frontend developer',
+                        'Tree planter',
+                        'Avid runner',
+                        'Tinkerer',
+                        'Lover of peanut butter',
+                        'DevOps enthousiast',
+                        'Gamer',
+                        'Software engineer',
+                    ]}
+                    multiTextDelay={3000}
+                    typeSpeed={70}
+                />
+            </div>
         </div>
     );
 }
