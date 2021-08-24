d53f3458e32ff0fba233727621b78399c1ca0279
janekkkk <janekkkk@users.noreply.github.com>
2021-08-24 12:46:51 +0000

    Deploying to gh-pages from @ janekkkk/portfolio@144df654296925a219f4ead8c9e9a3b98c76b04c 🚀

--- _next/static/IIXL0SAERQavKw0w3xs4F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/166-5cd2270f3888a34f9fcd.js","static/chunks/pages/index-2c960746e762e7ca3a84.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
