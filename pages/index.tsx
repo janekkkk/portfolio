544b3c8e4efc0b42c1c34ee602c807fe6fb43be9
Janek Ozga <janekozga1@gmail.com>
2021-10-12 17:05:00 +0200

    Add CV to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,15 @@ import Timeline from '@/features/timeline/timeline.component';
 
 const Index = (): JSX.Element => (
     <Layout>
-        <AboutMe className="mt-8 md:mt-12 mb-0" />
-        <Timeline className="mt-8 md:mt-12 mb-0" />
-        <Posts className="mt-12 md:mt-14" />
+        <div>
+            <AboutMe className="mt-8 md:mt-12 mb-0" />
+        </div>
+        <div>
+            <Timeline className="mt-8 md:mt-12 mb-0" />
+        </div>
+        <div>
+            <Posts className="mt-12 md:mt-14" />
+        </div>
     </Layout>
 );
 
