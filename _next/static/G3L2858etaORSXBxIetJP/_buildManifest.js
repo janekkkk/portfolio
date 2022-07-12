da2937fe1c10d456bb17600be4048d6e883a71f5
janekkkk <janekkkk@users.noreply.github.com>
2022-07-12 19:30:26 +0000

    Deploying to gh-pages from @ janekkkk/portfolio@1d9c1b3ccf5a4cd5ea9a53923348f53609da7b88 🚀

--- _next/static/G3L2858etaORSXBxIetJP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/515-bf1d76f785e8144d.js","static/chunks/pages/index-001c6d3db4ccd984.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/YJcdaEf55L-Gptz81mgII/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/100-c709497badd671e2.js","static/chunks/pages/index-6d963b3fc3485b19.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
