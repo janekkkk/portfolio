4069a688379be4aa5204985266b51f7497802749
Janek Ozga <janekozga@kateinnovations.com>
2022-07-12 20:40:01 +0200

    Update last major deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     mode: 'jit',
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './shared/**/*.{js,ts,jsx,tsx}', './features/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class', // or 'media' or 'class'
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './shared/**/*.{js,ts,jsx,tsx}', './features/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             fontFamily: {
