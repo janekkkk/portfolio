0e2e8acc6cca9e6689d9f9e825393973220dacb1
Janek Ozga <janekozga@kateinnovations.com>
2022-08-22 10:34:14 +0200

    fix: fix hmr

---
+++ pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { DefaultLayout } from '@/core/layout/default-layout.component';
+import React from 'react';
+
+export const Index = (): JSX.Element => {
+    return (
+        <DefaultLayout>
+            <h1>test</h1>
+        </DefaultLayout>
+    );
+};
+
+export default Index;
