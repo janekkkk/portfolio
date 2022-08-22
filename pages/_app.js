493a76fe9af09d8a081a8c7e1c0444cc789a2169
Janek Ozga <janekozga@kateinnovations.com>
2022-08-22 15:35:29 +0200

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../features/core/layout/styles.scss';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import mixpanel from 'mixpanel-browser';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const queryClient = new QueryClient();
+
+if (isProd) {
+    mixpanel.init('1dfa457dc710f12d7b9cb3cb3f74a7d2', { debug: true });
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const MyApp = ({ Component, pageProps }) => {
