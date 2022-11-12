cf71bc88e4e2cd2471613a7c01f82c38ed8cfafe
Janek <janekozga1@gmail.com>
2022-11-12 12:31:38 +0100

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withGoogleFonts } = require('nextjs-google-fonts');
-
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withGoogleFonts({
-    env: {
-        MIXPANEL_PROJECT_TOKEN: process.env.MIXPANEL_PROJECT_TOKEN,
-    },
+module.exports = {
     trailingSlash: true,
-    googleFonts: {
-        fonts: ['https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'],
-    },
-});
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import '../features/core/layout/styles.scss';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import mixpanel from 'mixpanel-browser';
+import '@fontsource/inter/';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const isProd = process.env.NODE_ENV === 'production';
+export const isProd = process.env.NODE_ENV === 'production';
 
 const queryClient = new QueryClient();
-
-if (isProd) {
-    mixpanel.init(process.env.MIXPANEL_PROJECT_TOKEN, { debug: !isProd });
-}
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const MyApp = ({ Component, pageProps }) => {
