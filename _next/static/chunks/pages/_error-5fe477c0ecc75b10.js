4ebec6bb59d8661dc0a1cd10931f65a6f0bbcbdd
janekkkk <janekkkk@users.noreply.github.com>
2025-02-02 15:58:04 +0000

    Deploying to gh-pages from @ janekkkk/portfolio@69baadc2fcdde8f5f525a40afe0145cb5634380d 🚀

---
+++ _next/static/chunks/pages/_error-5fe477c0ecc75b10.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{6541:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(2331)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(6541)),_N_E=_.O()}]);
